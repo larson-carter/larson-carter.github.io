16a6deee7c03163eb51cc2457805a852999174c3
Larson Carter <larsoncarter@blazermail.net>
2020-06-09 17:28:21 -0500

    Designate Home Route
    I designated the home route to be defined at /home instead of it inherently being known as "/"
    
    What will this do. This will alter the nav to have the basis home view of home. However when utilizing the nav it won't load /home/home or /home/about it will simply load it on the root folder!

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,14 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: '/home',
+    name: 'About',
+    component: Home
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( '../views/About.vue')
   }
 ]
 
